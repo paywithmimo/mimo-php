104a19e8330f0b1c5162d1c514066bdcdd655586
AtulWebline <atulrm@hotmail.com>
2012-11-30 18:44:52 +0530

    New Interface created for testing Search User & Transactions

--- examples/transaction.php
+++ examples/transaction.php
@@ -1,19 +1,55 @@
+<html>
+<head>
+  <title>Money Transfer : MIMO</title>
+</head>
+<body>
+<div>
+   
+    <a href="oauth.php">Home</a>
+    <br>
+    <a href="userinfo.php">User Profile</a> <br>
+<form name="userinfo_form" method="post" action="" >
+    <h1>Money Transfer</h1>
+    <br>
+    note : <input name="txtnote" type="text" id="txtnote">
+    <span id="VldNoteReq" style="display:none;">Please enter Note</span>
+    <br>
+    <br>
+    amount : <input name="txtAmount" type="text" id="txtAmount">
+    <span id="vldAmountReq" style="display:none;">Please enter Amount</span>
+    <br>
+    <br>
+    <input type="submit" name="btnAmount" value="Money Transfer" id="btnAmount">
+    <br>
+    </form>
+    </div>
 <?php
-// Include the Mimo REST Client
-require '../lib/MimoRestClient.php';
+if(isset($_POST['btnAmount'])){ 
+	$amount=$_POST['txtAmount'];
+	$note=$_POST['txtnote'];
+	// Include the Mimo REST Client
+	require '../lib/MimoRestClient.php';
 
-// Include any required keys
-require 'keys.php';
+	// Include any required keys
+	require 'keys.php';
 
-// Instantiate a new Mimo REST Client
-$Mimo = new MimoRestClient();
+	// Instantiate a new Mimo REST Client
+	$Mimo = new MimoRestClient();
 
-// Seed a previously generated access token
-$Mimo->setToken($token);
+	// Seed a previously generated access token
+	$Mimo->setToken($token);
 
-// Perform Transaction
-$transaction = $Mimo->transaction($amount='500',$note='Mimo Transfer Test');
-if(!$transaction) { $Mimo->getError(); }else{
-	print_r($transaction);
+	// Perform Transaction
+	$transaction = $Mimo->transaction($amount,$note);
+	if(!$transaction) { $Mimo->getError(); }else{
+		if(empty($transaction['Success'])){
+			echo $transaction['Message'];exit;
+		}else{
+			echo $transaction['Success'];exit;
+		}		
+	}
 }
 ?>
+
+</body>
+</html>
--- examples/userinfo.php
+++ examples/userinfo.php
@@ -1,19 +1,67 @@
+<html>
+<head>
+  <title>User Search : MIMO</title>
+</head>
+<body>
+
+<div>  <a href="oauth.php">Home</a>
+    <br>
+    <a href="transaction.php">Money Transfer</a><br>
+    <h1>User Profile</h1>
+    <br><form name="userinfo_form" method="post" action="" >
+    <table >
+	<tbody><tr>
+		<td><input id="rdblSearchParameter_0" type="radio" name="rdblSearchParameter" value="username" checked="checked"><label for="rdblSearchParameter_0">User Name</label></td>
+	</tr><tr>
+		<td><input id="rdblSearchParameter_1" type="radio" name="rdblSearchParameter" value="email"><label for="rdblSearchParameter_1">Email</label></td>
+	</tr><tr>
+		<td><input id="rdblSearchParameter_2" type="radio" name="rdblSearchParameter" value="phone"><label for="rdblSearchParameter_2">Phone</label></td>
+	</tr><tr>
+		<td><input id="rdblSearchParameter_3" type="radio" name="rdblSearchParameter" value="account_number"><label for="rdblSearchParameter_3">Account Number</label></td>
+	</tr>
+</tbody></table>
+    <br>
+    Enter search value :<input name="txtValue" type="text" id="txtValue">
+    <span id="VldValueReq" style="display:none;">Please enter serach parameter value</span>
+    <br>
+    <input type="submit" name="btnSubmit" value="Get User Profile"  id="btnSubmit"></form>
+    <br>
+    <br>
+   
+</div>
+
+
+
 <?php
-// Include the Mimo REST Client
-require '../lib/MimoRestClient.php';
-
-// Include any required keys
-require 'keys.php';
-
-// Instantiate a new Mimo REST Client
-$Mimo = new MimoRestClient();
-
-// Seed a previously generated access token
-$Mimo->setToken($token);
-
-// To get user account basic information
-$user_info = $Mimo->getUser($userField='username',$datastring='mimo-php');
-if(!$user_info) { $Mimo->getError(); }else{
-	print_r($user_info);
+if(isset($_POST['btnSubmit'])){
+	$field=$_POST['rdblSearchParameter'];
+	$datastring=$_POST['txtValue'];
+	// Include the Mimo REST Client
+	require '../lib/MimoRestClient.php';
+	
+	// Include any required keys
+	require 'keys.php';
+	
+	// Instantiate a new Mimo REST Client
+	$Mimo = new MimoRestClient();
+	
+	// Seed a previously generated access token
+	$Mimo->setToken($token);
+	
+	// To get user account basic information
+	$user_info = $Mimo->getUser($userField=$field,$datastring=$datastring);
+	if(!$user_info) { $Mimo->getError(); }else{
+		if(!empty($user_info['account_number'])){
+			print_r($user_info);
+		}
+		if(empty($user_info['Success'])){
+		echo $user_info['Message'];exit;
+		}else{
+			print_r($user_info);
+		}
+	}
 }
 ?>
+
+</body>
+</html>
