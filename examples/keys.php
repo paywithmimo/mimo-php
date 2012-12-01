16b1d14b1f8f3aba7fe560ed944ed01f66f4e494
atulwebline <atulrm@hotmail.com>
2012-12-01 14:18:57 +0530

    Example Completed

--- examples/keys.php
+++ examples/keys.php
@@ -4,7 +4,7 @@ session_start();
 $apiKey='NfXwj_-nso1NYdpZ';
 $apiSecret='xv-lHx9FusqgBWbEWkjDSn5x';
 $permissions='';
-$redirectUri = 'http://192.168.101.111:641/examples/oauth.php';
+$redirectUri = '/examples/oauth.php';
 if(isset($_SESSION['token']))
 	$token=$_SESSION['token'];
 else
