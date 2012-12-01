995bac4d3a17249caa7b71c3804a2138eb276030
atulwebline <atulrm@hotmail.com>
2012-12-01 14:20:18 +0530

    Example Completed

--- examples/userinfo.php
+++ examples/userinfo.php
@@ -3,13 +3,10 @@
   <title>User Search : MIMO</title>
 </head>
 <body>
-
-<div>  <a href="oauth.php">Home</a>
-    <br>
-    <a href="transaction.php">Money Transfer</a><br>
     <h1>User Profile</h1>
     <br><form name="userinfo_form" method="post" action="" >
-    <table >
+
+<table >
 	<tbody><tr>
 		<td><input id="rdblSearchParameter_0" type="radio" name="rdblSearchParameter" value="username" checked="checked"><label for="rdblSearchParameter_0">User Name</label></td>
 	</tr><tr>
@@ -29,9 +26,6 @@
     <br>
    
 </div>
-
-
-
 <?php
 if(isset($_POST['btnSubmit'])){
 	$field=$_POST['rdblSearchParameter'];
@@ -47,16 +41,22 @@ if(isset($_POST['btnSubmit'])){
 	
 	// Seed a previously generated access token
 	$Mimo->setToken($token);
-	
+
 	// To get user account basic information
-	$user_info = $Mimo->getUser($userField=$field,$datastring=$datastring);
+	$user_info = $Mimo->getUser($field,$datastring);
 	if(!$user_info) { $Mimo->getError(); }else{
 		if(!empty($user_info['account_number'])){
 			print_r($user_info);
 		}
 		if(empty($user_info['Success'])){
-		echo $user_info['Message'];exit;
-		}else{
+			if(isset($user_info['Message']))
+			{
+				echo $user_info['Message'];
+				exit;	
+			}
+			
+		}
+		else{
 			print_r($user_info);
 		}
 	}
